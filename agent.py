630d0e72a208bca2dedd590325e4ad416226d408
Skydzl <814101562@qq.com>
2023-07-07 10:33:16 +0800

    zby added

--- agent.py
+++ agent.py
@@ -1,8 +1,11 @@
 import numpy as np
 import torch
 import torch.nn as nn
+from collections import deque
+from torch.distributions import Bernoulli
 
-from model import Qnet
+from model import Qnet, PolicyNet
+
 
 class WorkerAgent(nn.Module):
     def __init__(self, config) -> None:
@@ -18,3 +21,75 @@ class WorkerAgent(nn.Module):
     def take_action(self, state):
         worker_history, action_list = state
         project_index_list = list()
+
+
+class MemoryQueue:
+    def __init__(self):
+        self.buffer = deque()
+    
+    def push(self, transitions):
+        self.buffer.append(transitions)
+        
+    def sample(self):
+        batch = list(self.buffer)
+        return zip(*batch)
+    
+    def clear(self):
+        self.buffer.clear()
+        
+    def __len__(self):
+        return len(self.buffer)
+
+
+class PolicyGradient:
+    "PolicyGradient智能体对象"
+    def __init__(self, memory, config):
+        self.gamma = config.gamma
+        self.memory = memory
+        # 策略网络
+        self.policy_net = PolicyNet()
+        # 优化器     
+        self.optimizer = torch.optim.RMSprop(self.policy_net.parameters(), lr=config.lr)
+        
+    def sample_action(self, state):
+        probs = self.policy_net(state)   
+        m = Bernoulli(probs)
+        action = m.sample()
+        action = int(action.item())
+        return action
+    
+    def update(self):
+        state_pool, action_pool, reward_pool = self.memory.sample()
+        state_pool, action_pool, reward_pool = list(state_pool), list(action_pool), list(reward_pool)
+        # 对奖励进行修正，考虑未来，并加入衰减因子
+        running_add = 0
+        for i in reversed(range(len(reward_pool))):
+            if reward_pool[i] == 0:
+                running_add = 0
+            else:
+                running_add = running_add * self.gamma + reward_pool[i]
+                reward_pool[i] = running_add
+                
+        # reawrd_mean = np.mean(reward_mean)
+        # reward_std = np.std(reward_pool)  # 求奖励标准差
+        # for i in range(len(reward_pool)):
+        #     # 标准化奖励
+        #     reward_pool[i] = (reward_pool[i] - reward_mean) / reward_std
+        
+        # 梯度下降
+        self.optimizer.zero_grad()
+        for i in range(len(reward_pool)):
+            state = state_pool[i]
+            action = action_pool[i]
+            reward = reward_pool[i]
+            probs = self.policy_net(state)
+            m = Bernoulli(probs)
+            # 加权(reward)损失函数，加负号(将最大化问题转化为最小化问题)
+            loss = -m.log_prob(action) * reward
+            loss.backward()
+        self.optimizer.step()
+        self.memory.clear()
+        
+        
+        
+        
--- model.py
+++ model.py
@@ -26,6 +26,7 @@ class ProjectEncoder(nn.Module):
         ), dim=-1)
         fc_out = self.fc(fc_input)
         return fc_out
+    
 
 class Qnet(nn.Module):
     def __init__(self, len_category, len_sub_category, len_industry, dim) -> None:
@@ -43,3 +44,22 @@ class Qnet(nn.Module):
         mlp_input = torch.cat([action_out, worker_out], dim=-1)
         mlp_out = self.mlp(mlp_input)
         return mlp_out
+
+
+class PolicyNet(nn.Module):
+    "策略网络（全连接网络）"
+    def __init__(self, input_dim, output_dim, hidden_dim=128):
+        """ 初始化策略网络，为全连接网络
+            input_dim: 输入的特征数即环境的状态维度
+            output_dim: 输出的动作维度
+        """
+        super(PolicyNet, self).__init__()
+        self.fc1 = nn.Linear(input_dim, hidden_dim)  # 输入层
+        self.fc2 = nn.Linear(hidden_dim, hidden_dim)  # 隐藏层
+        self.fc3 = nn.Linear(hidden_dim, output_dim)  # 输出层
+
+    def forward(self, x):
+        # 各层对应的激活函数
+        x = torch.relu(self.fc1(x))
+        x = torch.relu(self.fc2(x))
+        return torch.sigmoid(self.fc3(x))
