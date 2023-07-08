3940811664e98f4ec7b29fac650fa5ac10104b16
Rea1axb <1527039517@qq.com>
2023-07-08 13:30:09 +0800

    add empty_weight init

--- model.py
+++ model.py
@@ -1,6 +1,8 @@
 import torch
 import torch.nn as nn
 
+from utils import model_init
+
 class ProjectEncoder(nn.Module):
     def __init__(self, len_category, len_sub_catetory, len_industry, dim) -> None:
         super(ProjectEncoder, self).__init__()
@@ -13,8 +15,8 @@ class ProjectEncoder(nn.Module):
             nn.Linear(dim * 4, dim)
         )
     
-    def forward(self, action):
-        discrete_data, continuous_data = action
+    def forward(self, project):
+        discrete_data, continuous_data = project
         category, sub_category, industry = discrete_data
         average_score, client_feedback, total_awards_and_tips = continuous_data
         fc_input = torch.cat((
@@ -33,12 +35,14 @@ class Qnet(nn.Module):
     def __init__(self, len_category, len_sub_category, len_industry, dim) -> None:
         super(Qnet, self).__init__()
         self.project_encoder = ProjectEncoder(len_category, len_sub_category, len_industry, dim)
-        self.empty_weight = nn.Parameter(torch.Tensor(dim))
+        self.empty_weight = nn.Parameter(torch.Tensor(dim), requires_grad=True)
+        self.empty_weight.data.uniform_(-1, 1)
         self.mlp = nn.Sequential(
             nn.Linear(dim * 2, dim * 4),
             nn.ReLU(),
             nn.Linear(dim * 4, 1)
         )
+        model_init(self)
     
     def forward(self, worker_history, action):
         action_out = self.project_encoder(action)
